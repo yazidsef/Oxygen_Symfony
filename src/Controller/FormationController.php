01493c80c7df95ddc6944f269fa9ec410dd914b7
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-02-22 12:54:07 +0100

    convert the show function to param convert

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -5,19 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\CourseRepository;
 use App\Entity\Course;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class FormationController extends AbstractController
 {
-    #[Route('/formation/{id}', name: 'app_formation')]
+    #[Route('/formation/{id}/', name: 'app_formation')]
     public function show(Course $course): Response
     {
-        
-        
         return $this->render('formation/index.html.twig', ['formation' => $course]);
     }
-
-
 }
