7ef998a2fe4d29babf06045ab5b7865edee6a816
assma8jaballah <assma.jaballah@gmail.com>
2024-02-22 15:27:18 +0100

    loading sccs files

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,9 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('footer', './assets/footer.js')
+    .addEntry('formation', './assets/formation.js')
+    .addEntry('discipline', './assets/discipline.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -68,15 +71,15 @@ Encore
     // uncomment if you use TypeScript
     // .enableTypeScriptLoader()
 
-// uncomment if you use React
-// .enableReactPreset()
+    // uncomment if you use React
+    // .enableReactPreset()
 
-// uncomment to get integrity="..." attributes on your script & link tags
-// requires WebpackEncoreBundle 1.4 or higher
-// .enableIntegrityHashes(Encore.isProduction())
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    // .enableIntegrityHashes(Encore.isProduction())
 
-// uncomment if you're having problems with a jQuery plugin
-// .autoProvidejQuery()
+    // uncomment if you're having problems with a jQuery plugin
+    // .autoProvidejQuery()
 
     // enables Sass/SCSS support
     .enableSassLoader();
