174c1e23551b7f165f5ba685a1fc2c184b20556c
TRINH <laser2u02@gmail.com>
2024-02-25 02:58:41 +0100

    hero and discipline

--- assets/home.js
+++ assets/home.js
@@ -6,7 +6,54 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/home/home.scss';
+import './styles/home/hero.scss';
+import './styles/home/formation.scss';
+import './styles/home/card-dis.scss';
+
+
+import Splide from '@splidejs/splide';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+
+// SETUP OPTION SLIDE SHOW
+new Splide( '.splide', {
+    type   : 'carousel',
+    pagination: true,
+    perPage: 4,
+    focus: 'center',
+    autoplay: false,
+    arrows: false,     
+    breakpoints: {
+        1200: {
+            perPage: 3,
+        },
+        767: {
+            perPage: 1,
+        },
+    },
+    gap: '1.5rem',
+}).mount();
+
+// SETUP OPTION SECOND SLIDE
+
+// const splide2 = new Splide( '#course', {
+//     type   : 'carousel',
+//     pagination: false,
+//     perPage: 5,
+//     focus: 'center',
+//     breakpoints: {
+//         1200: {
+//             perPage: 3,
+//         },
+//         767: {
+//             perPage: 1,
+//         },
+//     },
+//     gap: '1.5rem',
+//     arrows: true,     
+
+// });
+// splide2.mount();
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\DisciplineRepository;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(
+        DisciplineRepository $disciplineRepository
+    ): Response
     {
-        return $this->render('home/index.html.twig');
+        // load all disciplines
+        // ...  
+        $disciplines = $disciplineRepository->findAll();
+        return $this->render('home/index.html.twig', 
+        [
+            'disciplines' => $disciplines,
+        ]);
     }
 }
