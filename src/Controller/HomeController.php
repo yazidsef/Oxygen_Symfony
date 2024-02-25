2f3930d4590951e4f5c77259b4dbf2e7448a3a3d
TRINH <laser2u02@gmail.com>
2024-02-25 02:59:17 +0100

    fix errors

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,20 +7,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\DisciplineRepository;
 
-
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(
         DisciplineRepository $disciplineRepository
-    ): Response
-    {
+    ): Response {
         // load all disciplines
-        // ...  
+        // ...
         $disciplines = $disciplineRepository->findAll();
-        return $this->render('home/index.html.twig', 
-        [
+        return $this->render(
+            'home/index.html.twig',
+            [
             'disciplines' => $disciplines,
-        ]);
+            ]
+        );
     }
 }
