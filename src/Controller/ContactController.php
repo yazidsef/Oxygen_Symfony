25616bd134b0df8345622fa55a0ed1a8b9999bba
assma8jaballah <assma.jaballah@gmail.com>
2024-02-26 15:45:50 +0100

    contact controller done

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -16,20 +16,26 @@ class ContactController extends AbstractController
     #[Route('/contact', name: 'app_contact')]
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        // creation nouveau contact
         $contact = new Contact();
-        $form = $this->createForm(ContactFormulaireType::class, $contact);
-        $form->handleRequest($request);
+        // creation formulaire de contact
+        $contactForm = $this->createForm(ContactFormulaireType::class, $contact);
+        //  Traitement de la requete
+        $contactForm->handleRequest($request);
+        //  Verification de la soumission du formulaire
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($contactForm->isSubmitted() && $contactForm->isValid()) {
             $entityManager->persist($contact);
             $entityManager->flush();
-
+        
+        //  Message de confirmation
             $this->addFlash('success', 'Merci ! nous avons bien reçu votre message, 
             nous vous répondrons dans les plus brefs délais.');
+        //redirection
 
             return $this->redirectToRoute('app_contact');
         }
 
-        return $this->render('contact/contact.html.twig', ['contact' => $contact , 'form' => $form]);
+        return $this->render('contact/contact.html.twig', ['contact' => $contact , 'form' => $contactForm]);
     }
 }
